ab2e2fa2980773f2749890a271003e3227ac9e9e
Arian <arian.kulmer@web.de>
2025-07-24 14:31:49 +0200

    malmo: Build IPACM
    Change-Id: I43cce0011bfc2381fdfc53202baf602a08852555

--- device.mk
+++ device.mk
@@ -98,6 +98,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    IPACM_Filter_cfg.xml
+
 # Keystore
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
